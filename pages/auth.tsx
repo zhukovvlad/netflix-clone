ea30a218afba87c13a901ffe62fc38ee49cc43df
Vladimir Zhukov <zhukovvlad@yandex.ru>
2023-07-13 13:54:31 +0300

    Added register function in auth.page

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,5 +1,6 @@
 import { useCallback, useState } from "react";
 import Input from "../components/Input";
+import axios from "axios";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
@@ -12,6 +13,18 @@ const Auth = () => {
     setVariant((currentVariant) =>
       currentVariant === "login" ? "register" : "login"
     );
+  }, []);
+
+  const register = useCallback(async () => {
+    try {
+      await axios.post("/api/register", {
+        email,
+        name,
+        password
+      })
+    } catch (error) {
+      console.log(error)
+    }
   }, []);
 
   return (
