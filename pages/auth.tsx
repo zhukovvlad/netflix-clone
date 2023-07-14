cd3499dc7c18e5a6124ceb94d2dd44103eb0492c
Vladimir Zhukov <zhukovvlad@yandex.ru>
2023-07-14 17:43:19 +0300

    Added google auth in to the app

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -2,11 +2,14 @@ import axios from "axios";
 import { useCallback, useState } from "react";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/router";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 
 import Input from "../components/Input";
 
 const Auth = () => {
   const router = useRouter();
+
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
@@ -63,6 +66,7 @@ const Auth = () => {
               {variant === "register" && (
                 <Input
                   id="name"
+                  type="text"
                   onChange={(event: any) => setName(event.target.value)}
                   value={name}
                   label="Username"
@@ -76,6 +80,7 @@ const Auth = () => {
                 type="email"
               />
               <Input
+                type="password"
                 id="password"
                 onChange={(event: any) => setPassword(event.target.value)}
                 value={password}
@@ -88,6 +93,42 @@ const Auth = () => {
             >
               {variant === "login" ? "Login" : "Sign Up"}
             </button>
+            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+              <div
+                onClick={() => signIn("google", { callbackUrl: "/" })}
+                className="
+                  w-10
+                  h-10
+                  bg-white
+                  rounded-full
+                  flex
+                  items-center
+                  justify-center
+                  cursor-pointer
+                  hover:opacity-80
+                  transition
+                "
+              >
+                <FcGoogle size="30" />
+              </div>
+              <div
+                onClick={() => signIn("github", { callbackUrl: "/" })}
+                className="
+                  w-10
+                  h-10
+                  bg-white
+                  rounded-full
+                  flex
+                  items-center
+                  justify-center
+                  cursor-pointer
+                  hover:opacity-80
+                  transition
+                "
+              >
+                <FaGithub size="30" />
+              </div>
+            </div>
             <p className="text-neutral-500 mt-12">
               {variant === "login"
                 ? "First time using Netflix?"
