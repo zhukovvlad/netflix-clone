959392b913013c18dad38a7ec854842dc6c79354
Vladimir Zhukov <zhukovvlad@yandex.ru>
2023-07-14 21:29:42 +0300

    Added function for check if user authenticated or not

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,37 +1,35 @@
-import axios from "axios";
-import { useCallback, useState } from "react";
-import { signIn } from "next-auth/react";
-import { useRouter } from "next/router";
-import { FcGoogle } from "react-icons/fc";
-import { FaGithub } from "react-icons/fa";
+import axios from 'axios';
+import { useCallback, useState } from 'react';
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import { FcGoogle } from 'react-icons/fc';
+import { FaGithub } from 'react-icons/fa';
 
-import Input from "../components/Input";
+import Input from '../components/Input';
 
 const Auth = () => {
   const router = useRouter();
 
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
 
-  const [variant, setVariant] = useState("login");
+  const [variant, setVariant] = useState('login');
 
   const toggleVariant = useCallback(() => {
-    setVariant((currentVariant) =>
-      currentVariant === "login" ? "register" : "login"
-    );
+    setVariant((currentVariant) => (currentVariant === 'login' ? 'register' : 'login'));
   }, []);
 
   const login = useCallback(async () => {
     try {
-      await signIn("credentials", {
+      await signIn('credentials', {
         email,
         password,
         redirect: false,
-        callbackUrl: "/",
+        callbackUrl: '/',
       });
 
-      router.push("/");
+      router.push('/');
     } catch (error) {
       console.log(error);
     }
@@ -39,7 +37,7 @@ const Auth = () => {
 
   const register = useCallback(async () => {
     try {
-      await axios.post("/api/register", {
+      await axios.post('/api/register', {
         email,
         name,
         password,
@@ -60,10 +58,10 @@ const Auth = () => {
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
             <h2 className="text-white text-4xl mb-8 font-semibold">
-              {variant === "login" ? "Sign In" : "Register"}
+              {variant === 'login' ? 'Sign In' : 'Register'}
             </h2>
             <div className="flex flex-col gap-4">
-              {variant === "register" && (
+              {variant === 'register' && (
                 <Input
                   id="name"
                   type="text"
@@ -88,14 +86,14 @@ const Auth = () => {
               />
             </div>
             <button
-              onClick={variant === "login" ? login : register}
+              onClick={variant === 'login' ? login : register}
               className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
             >
-              {variant === "login" ? "Login" : "Sign Up"}
+              {variant === 'login' ? 'Login' : 'Sign Up'}
             </button>
             <div className="flex flex-row items-center gap-4 mt-8 justify-center">
               <div
-                onClick={() => signIn("google", { callbackUrl: "/" })}
+                onClick={() => signIn('google', { callbackUrl: '/' })}
                 className="
                   w-10
                   h-10
@@ -112,7 +110,7 @@ const Auth = () => {
                 <FcGoogle size="30" />
               </div>
               <div
-                onClick={() => signIn("github", { callbackUrl: "/" })}
+                onClick={() => signIn('github', { callbackUrl: '/' })}
                 className="
                   w-10
                   h-10
@@ -130,14 +128,12 @@ const Auth = () => {
               </div>
             </div>
             <p className="text-neutral-500 mt-12">
-              {variant === "login"
-                ? "First time using Netflix?"
-                : "Already have an account?"}
+              {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}
               <span
                 onClick={toggleVariant}
                 className="text-white ml-1 hover:underline cursor-pointer"
               >
-                {variant === "login" ? "Create an account" : "Login"}
+                {variant === 'login' ? 'Create an account' : 'Login'}
               </span>
             </p>
           </div>
