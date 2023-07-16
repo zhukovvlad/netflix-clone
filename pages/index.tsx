e367182067ce226972c297ab93b6552f0fa66c44
Vladimir Zhukov <zhukovvlad@yandex.ru>
2023-07-16 13:58:45 +0300

    Initiated Profile page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import { NextPageContext } from 'next';
 import { getSession, signOut } from 'next-auth/react';
 
+import useCurrentUser from '../hooks/useCurrentUser';
+
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
 
@@ -19,9 +21,12 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
+  const { data: user } = useCurrentUser();
+
   return (
     <>
       <h1 className="text-2xl text-green-500">Hello Netflix</h1>
+      <p className="text-white">Logged in as {user?.name}</p>
       <button className="h-10 w-full bg-white" onClick={() => signOut()}>
         Logout!
       </button>
